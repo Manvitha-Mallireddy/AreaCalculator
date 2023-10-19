51290b65a55f8c3471418c500d49164ec7ba7aa4
AdmiN <AdmiN@DESKTOP-I89KIRV>
2023-10-19 06:29:52 +0530

    code updated to branch g

--- src/AreaCalc/Area.java
+++ src/AreaCalc/Area.java
@@ -4,7 +4,11 @@ public class Area {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		AreaCalc a=new AreaCalc();
+		a.circle(5);
+		a.rectangle(5, 7);
+		a.triangle(5, 7);
 	}
 
 }
